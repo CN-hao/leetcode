09a61fe80d8867fb1c90f9828e1b935e9d514c8c
lwh <526558805@qq.com>
2025-01-15 15:56:52 +0800

    20250115

---
+++ 3066.minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=3066 lang=cpp
+ * @lcpr version=30204
+ *
+ * [3066] 超过阈值的最少操作数 II
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        std::priority_queue<unsigned long long , std::vector<unsigned long long>, std::greater<unsigned long long>> 
+            min_heap(nums.begin(), nums.end());
+        int c=0;
+        while(min_heap.top() < k) {
+            unsigned long long a = min_heap.top();
+            min_heap.pop();
+            unsigned long long b = min_heap.top();
+            min_heap.pop();
+            min_heap.push(a *2+ b);
+            c++;
+        }
+        return c;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [2,11,10,1,3]\n10\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,1,2,4,9]\n20\n
+// @lcpr case=end
+
+ */
+
