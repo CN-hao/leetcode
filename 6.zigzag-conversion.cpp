597ecefac58d9d3c0069fc2d84300e13b7a87c62
lwh <526558805@qq.com>
2025-06-25 12:47:36 +0800

    250625抄了一手

---
+++ 6.zigzag-conversion.cpp
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=6 lang=cpp
+ * @lcpr version=30204
+ *
+ * [6] Z 字形变换
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.size()) {
+            return s;
+        }
+
+        vector<string> rows(min(numRows, int(s.size())));
+        int curRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[curRow] += c;
+            if (curRow == 0) {
+                goingDown = true;
+            } else if (curRow == numRows - 1) {
+                goingDown = false;
+            }
+            curRow += goingDown ? 1 : -1;
+        }
+
+        string result;
+        for (const string& row : rows) {
+            result += row;
+        }
+        return result;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "PAYPALISHIRING"\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "PAYPALISHIRING"\n4\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "A"\n1\n
+// @lcpr case=end
+
+ */
+
