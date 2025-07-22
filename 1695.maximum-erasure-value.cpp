c144c5444c05573bfa9fd746858f2c174dfde19f
lwh <526558805@qq.com>
2025-07-22 17:33:55 +0800

    超水混过

---
+++ 1695.maximum-erasure-value.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=1695 lang=cpp
+ * @lcpr version=30204
+ *
+ * [1695] 删除子数组的最大得分
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        unordered_map<int,int> us; 
+        int beg=0,ans=0,temp=0;
+        for(int i=0;i<nums.size();++i){
+            if(us.find(nums[i])!=us.end()){
+                int mid=us[nums[i]];
+                temp=0;
+                for(int j=beg;j<i;++j){
+                    temp+=nums[j];
+                    if(j<mid)us.erase(nums[j]);
+                }
+                ans=max(ans,temp);
+                beg=mid+1;
+            }
+            us[nums[i]]=i;
+        }
+        temp=0;
+        for(int j=beg;j<nums.size();++j){
+            temp+=nums[j];
+        }
+        ans=max(ans,temp);
+        return ans;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [4,2,4,5,6]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [5,2,1,2,5,2,1,2,5]\n
+// @lcpr case=end
+
+ */
+
