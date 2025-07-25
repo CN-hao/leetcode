65f6592108d09a8780fcc6e7ed70ccaf6fd586a7
lwh <526558805@qq.com>
2025-07-25 18:02:28 +0800

    250713+1

---
+++ 1025.divisor-game.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=1025 lang=cpp
+ * @lcpr version=30204
+ *
+ * [1025] 除数博弈
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    bool divisorGame(int n) {
+        return n%2==0;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n
+// @lcpr case=end
+
+ */
+
